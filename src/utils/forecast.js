bfb3a130f4ff1e927ea10601dcee4902ed4ab273
Ignasi A <ignasi.ardevol@gmail.com>
2020-08-16 13:34:25 +0200

    Preparat per Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,19 @@
-const request = require('request')
+const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const weatherstackKey = 'f6c37bff323393d4eb11e8ded4d75f69'
+    const url = `http://api.weatherstack.com/current?access_key=${weatherstackKey}&query=${latitude},${longitude}`
 
+    // request(objOptions, callback);
+    //request({ url }, (error, response) => {})
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            //console.log(body.current);
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.weather_descriptions + '. Humidity: '+body.current.humidity)
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,9 +1,34 @@
-const request = require('request')
+const request = require('postman-request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const token = 'pk.eyJ1IjoiaWduYXNpNjIiLCJhIjoiY2tkcjZpc2wyMWJzYjJxdHZ6OXVwbnowcyJ9.43NZLV0a9bt8UuYsRB6Rfg'
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${token}&limit=1`
 
+
+    // const request = require('request');
+ 
+    // const options = {
+    //   url: 'https://api.github.com/repos/request/request',
+    //   headers: {
+    //     'User-Agent': 'request'
+    //   }
+    // };
+     
+    // function callback(error, response, body) {
+    //   if (!error && response.statusCode == 200) {
+    //     const info = JSON.parse(body);
+    //     console.log(info.stargazers_count + " Stars");
+    //     console.log(info.forks_count + " Forks");
+    //   }
+    // }
+     
+    // request(options, callback);
+    //request({ url }, (error, response) => {})
+
+    // destructurem boy de response
     request({ url, json: true }, (error, { body }) => {
+        //console.log(body);
+        
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
